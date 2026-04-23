e7a887d8186fb7e22abcfa5b95aeea0cbf8f48d4
Teerachaya <chaya.fact@gmail.com>
2026-04-24 03:07:09 +0700

    fix-bug: loading height

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import Spinner from '@/components/ui/Spinner';
 
 export default function Loading() {
   return (
-    <div className="min-h-[60vh] flex items-center justify-center bg-gray-50">
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="flex flex-col items-center gap-5 px-8 py-6">
         
         {/* Spinner */}
